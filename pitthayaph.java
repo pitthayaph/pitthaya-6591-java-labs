17d0a0a27dfef9e420b258ce995bfbfb97041062
pitthayaph <151108007+pitthayaph@users.noreply.github.com>
2023-11-17 10:48:32 +0700

    Update pitthayaph.java
    update

--- pitthayaph.java
+++ pitthayaph.java
@@ -3,5 +3,6 @@ public class pitthayaph {
     public static void main(String[] args) {
         System.out.println(x:"Hello World") ;
         System.out.println(x:"Meow Meow Meow") ; //bark!
+        System.out.println(x:"Update Text") ; //New Text!!!
     }
 }
