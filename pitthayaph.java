af2cc3410140201f8c201f8b0bffbb25681ecb30
Pitthaya <pitthaya.ph@kkumail.com>
2023-11-17 11:38:46 +0700

    New

--- pitthayaph.java
+++
@@ -1,8 +0,0 @@
-public class pitthayaph {
-    Run | Debug
-    public static void main(String[] args) {
-        System.out.println(x:"Hello World") ;
-        System.out.println(x:"Meow Meow Meow") ; //bark!
-        System.out.println(x:"Update Text") ; //New Text!!!
-    }
-}
